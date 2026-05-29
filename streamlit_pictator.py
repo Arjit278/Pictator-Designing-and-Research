48c0ffb2d2a02ff0a493e691f1c6ba1da71b32c0
Arjit278 <arjit278@gmail.com>
2026-05-29 17:30:53 +0530

    Fix password format in authentication check

--- streamlit_pictator.py
+++ streamlit_pictator.py
@@ -40,7 +40,7 @@ with st.sidebar:
         user = st.text_input("Username")
         pwd = st.text_input("Password", type="password")
         if st.button("Login"):
-            if user == "Harmony" and pwd == "Harmony_Pictator123":
+            if user == "Harmony" and pwd == "Harmony-Pictator123":
                 st.session_state.authenticated = True
                 st.rerun()
             else:
